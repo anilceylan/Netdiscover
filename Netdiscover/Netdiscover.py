898ab7e3d697243ec1f28126566258839fbf93b8
anilceylan <a.ceylan2649@gmail.com>
2022-12-04 21:46:20 +0300

    Netdiscover code with Python.
    It is a Netdiscover code written in Python, inspired by Atıl Samancıoğlu.

---
+++ Netdiscover/Netdiscover.py
@@ -0,0 +1,31 @@
+import argparse
+import scapy.all as scapy
+
+class ARPPing():
+	
+	def __init__(self):
+		print('ARPPing Başlatıldı')
+
+	def get_user_input(self):
+
+		parser = argparse.ArgumentParser()
+		parser.add_argument('-i','--ipaddress', type=str , help='Enter your IP address')
+		args = parser.parse_args()
+		#print(args.ipaddress)
+		
+		if args.ipaddress != None:
+			return args
+		else:
+			print('Yanlış veya eksik giriş yaptınız')	
+		
+		
+	def arp_request(self,ip):
+		arp_request_packet = scapy.ARP(pdst=ip)
+		broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+		combined_packet = broadcast_packet/arp_request_packet
+		answered_list, unanswered_list = scapy.srp(combined_packet, timeout=2)
+		answered_list.summary()
+		
+arp_ping = ARPPing()
+user_input = arp_ping.get_user_input()
+arp_ping.arp_request(user_input.ipaddress)
